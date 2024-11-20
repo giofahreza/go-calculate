04fd22016cdfa0260dee73e4a4ecb7590ca0a34b
Giofahreza <giofahreza2000@gmail.com>
2024-11-20 20:20:13 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func mul(a, b int) int {
+	return a * b
+}
+
+func div(a, b int) int {
+	return a / b
+}
